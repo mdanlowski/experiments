90d6e7d28cd5766e385860fb823e7fe2fa527819
Martin <marcindanlowski@gmail.com>
2019-03-08 17:11:06 +0100

    Update regex. add diagnostics to translate_source

--- translate_source.py
+++ translate_source.py
@@ -5,7 +5,8 @@ import timeit
 start = timeit.default_timer()
 
 paths = "C:/Source/transfile_list.csv"
-rex  = ":.{0,1}'[A-Z]{1}[a-z].*'"
+rex = ":.{0,1}'[A-Z]{1}[A-ząćęłńóśżź\s].*'"
+
 
 def fetch_translation(lnum):
     with open("C:/Source/trans_csv.csv", 'r') as translated_source:
@@ -13,7 +14,8 @@ def fetch_translation(lnum):
         for l in translated_source_lines:
             # print(translated_source_lines.line_num, l)
             test = translated_source_lines.line_num == lnum
-            if test: return l[2]
+            if test:
+                return l[2]
             else: continue
             
 trans_counter = 1
@@ -22,13 +24,16 @@ with open(paths, 'r') as filepaths: # open address list
     for address in filepathsdata: # for each single file from address list
         with open(address[0], 'r') as fin:
             # single_file_data = csv.reader(csvfile, delimiter=';', quotechar='"')
-            fout = open("C:/Source./translated" + fin.name[fin.name.rfind('/'):], 'w')
+            fout = open("C:/Source/translated" + fin.name[fin.name.rfind('/'):], 'w')
             for line in fin.readlines():
+                try:
+                    fout.write(re.sub(rex, fetch_translation(trans_counter), line))
+                except TypeError:
+                    print(trans_counter)
                 if isinstance(re.search(rex, line), re.Match):
                     trans_counter += 1
-                fout.write(re.sub(rex, fetch_translation(trans_counter), line))
                 # print(trans_counter)
                 
             fout.close()
             
-print(timeit.default_timer() - start)
+print(timeit.default_timer() - start, "Sub count:", trans_counter)
