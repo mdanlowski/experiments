88e62a5e1fee7aaba5427425a44c65ac98663c11
Martin <marcindanlowski@gmail.com>
2019-03-08 13:57:33 +0100

    python json values translator
    a program devised at work to look for / : 'Abcd(...)' / through many files and produce files with these matches substituted with their translations. Translations source is a CSV file.
    - [ ] Planning to generalize this utility and
    - [ ] equip it with arguments parser

---
+++ translate_source.py
@@ -0,0 +1,34 @@
+import re
+import csv
+import timeit
+
+start = timeit.default_timer()
+
+paths = "C:/Source/transfile_list.csv"
+rex  = ":.{0,1}'[A-Z]{1}[a-z].*'"
+
+def fetch_translation(lnum):
+    with open("C:/Source/trans_csv.csv", 'r') as translated_source:
+        translated_source_lines = csv.reader(translated_source, delimiter=';', quotechar='"')
+        for l in translated_source_lines:
+            # print(translated_source_lines.line_num, l)
+            test = translated_source_lines.line_num == lnum
+            if test: return l[2]
+            else: continue
+            
+trans_counter = 1
+with open(paths, 'r') as filepaths: # open address list
+    filepathsdata = csv.reader(filepaths, delimiter=';', quotechar='"')
+    for address in filepathsdata: # for each single file from address list
+        with open(address[0], 'r') as fin:
+            # single_file_data = csv.reader(csvfile, delimiter=';', quotechar='"')
+            fout = open("C:/Source./translated" + fin.name[fin.name.rfind('/'):], 'w')
+            for line in fin.readlines():
+                if isinstance(re.search(rex, line), re.Match):
+                    trans_counter += 1
+                fout.write(re.sub(rex, fetch_translation(trans_counter), line))
+                # print(trans_counter)
+                
+            fout.close()
+            
+print(timeit.default_timer() - start)
